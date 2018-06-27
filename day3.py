94ec4119febaff24e96bb2a72e07bf4cde282bde
rmintz <raymond.mintz@verizondigitalmedia.com>
2018-06-27 10:37:14 -0700

    day 3 stuff

---
+++ day3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+print("Day 3 for Jerermiah")
+
+
+#Do some functions that take keyword arguments
+
+#Function that takes name and age, then says hi my name is .... from the keyword args
+
+
+#Learn class
+
+#create a class called Car
+
+#make a few cars from Car class, which has TOP_SPEED, MAKE, MODEL
+
+
+def monkey_trouble(a_smile, b_smile):
+  if a_smile  == b_smile:
+  	return True
+  else:
+  	return False
+
+
+
+def hello(hello):
+	print("hello {}.".format(hello))
+
+hello("GOODBYE")
+
+
+def jeremiah(name, age):
+	print("hello my name is {} and im {}.".format(jeremiah, 13))
+
+jeremiah(name=jeremiah,age=13)
